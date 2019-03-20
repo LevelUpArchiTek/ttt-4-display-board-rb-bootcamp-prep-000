fae0eadd17ca2ed13b65c50b5843b2414d5c2a84
TK <zeropercent@hotmail.com>
2019-03-20 00:33:35 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -193,7 +193,7 @@ describe "#display_board in 'lib/display_board.rb" do
        expect(rows[1]).to eq("-----------")
        expect(rows[2]).to eq(" O | O | O ")
        expect(rows[3]).to eq("-----------")
-       expect(rows[4]).to eq(" O | O | O ")
+       expect(rows[4]).to eq(" O | X | O ")
 
       expect(true).to be(true)
     end
